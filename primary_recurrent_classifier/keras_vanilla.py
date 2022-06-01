989e0ec6cc0c220670bcf9407007b19599be872b
Udbhav Ram <udbhavram41@gmail.com>
2022-06-01 08:45:49 -0400

    more validation work
    adding data to the validation set to improve late epoch val losses

---
+++ primary_recurrent_classifier/keras_vanilla.py
@@ -0,0 +1,85 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
+
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = '../data/keras_training/train'
+validation_data_dir = '../data/keras_training/validation'
+nb_train_samples = 254
+nb_validation_samples = 80
+epochs = 100
+batch_size = 16
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+# checkpoint = ModelCheckpoint("methylation_classifier", monitor='accuracy', verbose=2, save_best_only=True, save_weights_only=False, mode='auto', save_freq=1)
+# early = EarlyStopping(monitor='accuracy', min_delta=0.001, patience=20, verbose=2, mode='auto')
+tensorboard = TensorBoard()
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size, callbacks=[tensorboard])
+
+#  tensorboard --logdir logs/
+
+
+model.save_weights('first_try.h5')
